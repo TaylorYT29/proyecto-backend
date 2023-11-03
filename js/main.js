95a47d02a89a05dc0f89ff3d6a4c039f96160fab
TaylorYT29 <taylorduransibaja@gmail.com>
2023-11-02 21:10:41 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const prevButton = document.getElementById("prevBtn");
+const nextButton = document.getElementById("nextBtn");
+const carouselSlide = document.querySelector(".carousel-slide");
+
+let slideIndex = 0;
+let timer;
+
+prevButton.addEventListener("click", () => {
+    moveSlide(-1);
+    resetTimer();
+});
+
+nextButton.addEventListener("click", () => {
+    moveSlide(1);
+    resetTimer();
+});
+
+function moveSlide(n) {
+    slideIndex += n;
+    if (slideIndex < 0) {
+        slideIndex = 2;
+    } else if (slideIndex > 2) {
+        slideIndex = 0;
+    }
+
+    const translateX = -slideIndex * 100;
+    carouselSlide.style.transform = `translateX(${translateX}%)`;
+}
+
+function autoAdvance() {
+    moveSlide(1);
+    timer = setTimeout(autoAdvance, 3000); 
+}
+
+function resetTimer() {
+    clearTimeout(timer);
+    timer = setTimeout(autoAdvance, 3000);  
+}
+
+autoAdvance();
